f6ffd44e9cc5279a251e29ed9d2dbf88a7eac516
AksenovKirill <aksenovkirill@yandex.ru>
2022-02-11 17:00:29 +0300

    Render main-page component, small-film-card component and big-film-card component; add props in big-film-card

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,15 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainPage from '../main-page/main-page';
+
+type AppMainPageProps = {
+  filmTitle: string,
+  filmGenre: string,
+  releaseDate: number,
+}
+
+function App({filmTitle, filmGenre, releaseDate}: AppMainPageProps): JSX.Element {
+  return (
+    <MainPage filmTitle={filmTitle} filmGenre={filmGenre} releaseDate={releaseDate}/>
+  );
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const DataMainPage = {
+  filmTitle: 'The Grand Budapest Hotel',
+  filmGenre: 'Drama',
+  releaseDate: 2014,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      filmTitle = {DataMainPage.filmTitle}
+      filmGenre = {DataMainPage.filmGenre}
+      releaseDate = {DataMainPage.releaseDate}
+    />
   </React.StrictMode>,
   document.getElementById('root'));
