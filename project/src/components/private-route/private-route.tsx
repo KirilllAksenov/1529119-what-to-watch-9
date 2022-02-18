61418dd2e78dd401fe43a87377821be603313626
AksenovKirill <aksenovkirill@yandex.ru>
2022-02-18 21:46:59 +0300

    Delete RouteProps and add optimal type in private-route

--- project/src/components/private-route/private-route.tsx
+++ project/src/components/private-route/private-route.tsx
@@ -1,18 +1,17 @@
 import {Navigate} from 'react-router-dom';
-import {RouteProps} from 'react-router-dom';
 import {AppRoute, AuthorizationStatus} from '../../const';
 
-type PrivateRouteProps = RouteProps & {
+type PrivateRouteProps = {
   authorizationStatus: AuthorizationStatus;
   children: JSX.Element;
-}
+};
 
 function PrivateRoute(props: PrivateRouteProps): JSX.Element {
   const {authorizationStatus, children} = props;
 
   return (
     authorizationStatus === AuthorizationStatus.Auth
-      ? children
+      ? children as JSX.Element
       : <Navigate to={AppRoute.SingIn} />
   );
 }
