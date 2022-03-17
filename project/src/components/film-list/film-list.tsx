8fff90a26ac4915212bf94cf73ec82f88825ef0d
AksenovKirill <aksenovkirill@yandex.ru>
2022-03-17 20:40:38 +0300

    finished task

--- project/src/components/film-list/film-list.tsx
+++ project/src/components/film-list/film-list.tsx
@@ -13,10 +13,6 @@ type Props = {
 function FilmsList({films, genre, id}: Props): JSX.Element {
   const [activeFilmId, setActiveFilmId] = useState<number | null>(null);
 
-  if (genre) {
-    films = films.filter((film) => film.genre === genre && film.id !== id).slice(0, MAX_FILMS_COUNT);
-  }
-
   return (
     <div className="catalog__films-list">
       {films.map((film) => (
--- project/src/screens/main-screen/main-screen.tsx
+++ project/src/screens/main-screen/main-screen.tsx
@@ -6,13 +6,19 @@ import FilmsList from '../../components/film-list/film-list';
 import GenreList from '../../components/genre-list/genre-list';
 import ButtonShowMore from '../../components/button-show-more/button-show-more';
 import { useAppSelector } from '../../hooks';
+import { useEffect, useState } from 'react';
+import { DEFAULT_ACTIVE_GENRE } from '../../const';
 
 function MainScreen(): JSX.Element {
-  const genres = useAppSelector((state) => state.activeGenre);
-
+  const [genres, setGenres] = useState<string[]>([]);
+  const activeGenre = useAppSelector((state) => state.activeGenre);
   const films = useAppSelector((state) => state.films);
-
+  const filmsList = (activeGenre === 'All genres') ? films : films.filter(({genre}) => activeGenre === genre);
   const {name, backgroundImage } = films[0];
+  useEffect(() => {
+    setGenres([DEFAULT_ACTIVE_GENRE, ...new Set(films.map((film) => film.genre))]);
+  }, [films]);
+
   return (
     <>
       <section className="film-card">
@@ -38,7 +44,7 @@ function MainScreen(): JSX.Element {
           <ul className="catalog__genres-list">
             <GenreList genres={genres}/>
           </ul>
-          <FilmsList films={films}/>
+          <FilmsList films={filmsList}/>
           <ButtonShowMore/>
         </section>
         <Footer/>
